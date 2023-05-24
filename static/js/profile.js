ff72151b53f04f97289107906b221dfc1bbf2f24
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-24 19:34:07 +0900

    프로필 페이지

--- static/js/profile.js
+++ static/js/profile.js
@@ -4,11 +4,17 @@ window.onload = () => {
 }
 
 
-const user_id = new URLSearchParams(window.location.search).get('user_id');
-const loggedInUserId = localStorage.getItem("user_id");
+const user_id = parseInt(new URLSearchParams(window.location.search).get('user_id'));
 
-console.log(user_id)
-console.log(loggedInUserId)
+
+const payload = localStorage.getItem("payload");
+const payload_parse = JSON.parse(payload)
+
+const token = localStorage.getItem("access");
+const logined_id = parseInt(payload_parse.user_id);
+const account = payload_parse.account;
+
+
 
 async function Profile(user_id) {
 
@@ -17,19 +23,33 @@ async function Profile(user_id) {
     })
 
     response_json = await response.json()
+    const user_id_int = parseInt(user_id)
 
-    document.getElementById('account').innerText = response_json.account
     document.getElementById('nickname').innerText = response_json.nickname
     document.getElementById('introduce').innerText = response_json.introduce
-    document.getElementById('joined-at').innerText = response_json.joined_at
+    document.getElementById('article-count').innerText = response_json.article_count;
+    document.getElementById('followers-count').innerText = response_json.followers_count;
+    document.getElementById('following-count').innerText = response_json.following_count;
+    
 
-    if (user_id === loggedInUserId) {
+    if (user_id_int === logined_id) {
         // 해당 프로필 페이지가 로그인된 사용자의 것일 때 - 수정,탈퇴 보이기
         document.getElementById('edit-account').style.display = "block";
-        
+        document.getElementById('delete-account').style.display = "block";
+
+    }else{
+        document.getElementById('edit-account').style.display = "none";
     }
 }
 
+function toggleDeleteForm() {
+    var deleteForm = document.getElementById("delete-form");
+    if (deleteForm.style.display === "none") {
+        deleteForm.style.display = "block";
+    } else {
+        deleteForm.style.display = "none";
+    }
+}
 
 // 회원탈퇴
 async function deactivateAccount() {
@@ -47,9 +67,9 @@ async function deactivateAccount() {
                 "password": password
             })
         })
-        if (response.status == 204){
+        if (response.status == 204) {
             handleLogout()
-        }else{
+        } else {
             alert("비밀번호를 확인해주세요")
         }
 
