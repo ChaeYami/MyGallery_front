99e8dc11e8da2eb458ed3ddbed22870dacd2562b
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-26 00:52:02 +0900

    사이드바에 프로필 이미지 추가

--- static/js/right_sidebar.js
+++ static/js/right_sidebar.js
@@ -9,23 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 rightSidebarContainer.innerHTML = data;
 
-                
+
                 const payload = localStorage.getItem("payload");
                 const payload_parse = JSON.parse(payload);
-                intro.innerText = `${payload_parse.nickname}`;
-                intro.href = `${frontend_base_url}/user/profile.html?user_id=` + payload_parse.user_id;
+                intro.innerHTML = `
+                <a href="${frontend_base_url}/user/profile.html?user_id=${payload_parse.user_id}">
+                <span><img class="profile-img" src="${backend_base_url}/media/${payload_parse.profile_img}" alt=""></span>
+                ${payload_parse.nickname}</a>
+            `
 
-            
+
             })
             .catch(error => {
                 console.error("Error fetching navigation bar:", error);
             });
-    } 
+    }
 
 })
 
 //로그아웃
-async function handlelogout(){
+async function handlelogout() {
     localStorage.removeItem("access")
     localStorage.removeItem("refresh")
     localStorage.removeItem("payload")
