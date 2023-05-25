e965008745877743adefeac67d9f0a8e8b6fdf94
Lee Juneyoung <wnsdud318@gmail.com>
2023-05-26 00:52:33 +0900

    터지는 서버 잡고 실시간으로 볼 수 있음, post 기능 추가 중...

--- static/js/api.js
+++ static/js/api.js
@@ -8,6 +8,34 @@ $(document).ready(async function () {
 
 ////게시글 작성 수정////////////////////////////////////////////
 //이미지변환 post
+async function TransForm(change_id, formData) {
+    try {
+        const response = await $.ajax({
+            url: `${backend_base_url}/change/${change_id}/`,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('access')}`
+                // Authorization: `Bearer ${token}`
+            }
+        });        
+        
+        // 응답 데이터 처리
+        return response; // 결과 반환
+
+    } catch (error) {
+    console.error(error);
+    // window.location.href = "404.html";
+    }  
+};
+
 //게시글 post
+async function PostArticle() {
+
+}
+
+
 //get
 //put
