a72625032964a1cae9a8a526351b9ebabb1d8d0b
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-26 18:13:22 +0900

    base_url.js 분리!

--- static/js/api.js
+++ static/js/api.js
@@ -1,5 +1,4 @@
-const frontend_base_url = "http://127.0.0.1:5500"
-const backend_base_url = "http://127.0.0.1:8000"
+
 
 $(document).ready(async function () {
     let urlParam = new URLSearchParams(window.location.search);
--- static/js/left_sidebar.js
+++ static/js/left_sidebar.js
@@ -12,8 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 
                 const payload = localStorage.getItem("payload");
                 const payload_parse = JSON.parse(payload);
-                intro.innerText = `${payload_parse.nickname}`;
-                intro.href = `${frontend_base_url}/user/profile.html?user_id=` + payload_parse.user_id;
+                intro.innerHTML = `
+                <a href="${frontend_base_url}/user/profile.html?user_id=${payload_parse.user_id}">
+                <span><img class="profile-img" src="${backend_base_url}/media/${payload_parse.profile_img}" alt=""></span>
+                ${payload_parse.nickname}</a>
+            `
 
                 let navbarRight = document.getElementById("navbar-right");
                 let newLi = document.createElement("li");
@@ -49,3 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
 })
 
+//로그아웃
+async function handlelogout() {
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    localStorage.removeItem("payload")
+}
